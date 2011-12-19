d131a50a36b2375d9683c945b07eedb947bbd144
anthr6x <anthr6x@gmail.com>
2011-12-19 12:14:16 +0000

    second button added

--- TestVisualStudio/Form1.h
+++ TestVisualStudio/Form1.h
@@ -42,6 +42,7 @@ namespace TestVisualStudio {
 			}
 		}
 	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  button2;
 	protected: 
 
 	private:
@@ -58,6 +59,7 @@ namespace TestVisualStudio {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
@@ -70,11 +72,22 @@ namespace TestVisualStudio {
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
 			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(131, 204);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &Form1::button2_Click);
+			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 262);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Name = L"Form1";
 			this->Text = L"Form1";
@@ -88,6 +101,9 @@ namespace TestVisualStudio {
 				 /*
 				 just added some comment here
 				 */
+			 }
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+				 MessageBox::Show("another button added");
 			 }
 	};
 }
